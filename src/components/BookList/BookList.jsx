b1547a6ef478e6c304fedaf32aa2593702a0e08c
simonacossai <cossaisimona@gmail.com>
2020-11-18 15:55:50 +0100

    DONE- creating singlebook and passed it into booklist

---
+++ src/components/BookList/BookList.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Container, Row, Col, Card } from "react-bootstrap";
+import romance from '../../data/romance.json';
+import SingleBook from '../SingleBook/SingleBook';
+
+class BookList extends React.Component {
+
+    render(props) {
+        return (
+            <div>
+                <Container className=" justify-content-center mt-3 mb-4">
+                    <div className="d-flex align-items-center">
+                        <h1>BookList</h1>
+                    </div>
+                    <hr className="my-2" />
+                    <Row className="justify-content-start mt-3">
+                        {romance.map((romance) => (
+                            <SingleBook book={romance}/>
+                        ))}
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+}
+
+export default BookList;
---
+++ src/components/SingleBook/SingleBook.jsx
@@ -0,0 +1,32 @@
+/*Create a SingleBook component as a function. The component receives a
+book object as a prop, and displays the cover and the title of the book.
+Use react-bootstrap Cards to display a book (The book object can be read
+from the one of the .json book files we gave you yesterday)
+*/
+
+import React from 'react';
+import { Container, Row, Col, Card } from "react-bootstrap";
+import MyBadge from '../MyBadge/MyBadge';
+import './SingleBook.css';
+
+const SingleBook = (props) => {
+    console.log(props);
+    return (
+        <> 
+                <Col className="mt-3">
+                    <Card style={{ width: '18rem' }} className="card">
+                        <Card.Img variant="top" src={props.book.img} className="books-image" />
+                        <Card.Body>
+                            <Card.Title className="title" >{props.book.title}</Card.Title>
+                            <div className="d-flex justify-content-between align-items-center text-center mt-2">
+                                <MyBadge text={props.book.category} colour="success"></MyBadge>
+                                <small className="text-muted">${props.book.price}</small>
+                            </div>
+                        </Card.Body>
+                    </Card>
+                </Col>
+        </>
+    )
+};
+
+export default SingleBook;
